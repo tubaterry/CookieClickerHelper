cad5340d20a2b116e70af2677cf9339ef214f579
tubaterry <tuba.terry@gmail.com>
2013-09-23 00:09:57 -0600

    Let's do this GPL style.

--- cookieDataGrabber.js
+++ cookieDataGrabber.js
@@ -1,3 +1,20 @@
+/*
+	Copyright 2013, Chris Terry - tuba.terry@gmail.com
+	This file is part of Cookie Clicker Helper.
+
+    Cookie Clicker Helper is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cookie Clicker Helper is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cookie Clicker Helper.  If not, see <http://www.gnu.org/licenses/>.
+*/
 var timer = 0;
 var cookieBankCounter = 0;
 
