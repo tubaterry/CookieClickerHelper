0ed1e6cc4c1c9980135625dd4a3be8def569a510
tubaterry <tuba.terry@gmail.com>
2013-09-23 00:04:34 -0600

    Matches version on Chrome Web Store
    https://chrome.google.com/webstore/detail/cookie-clicker-helper/iagobhdnbhalgapgknhklgieiehcjlcn?hl=en&gl=US

---
+++ cookieDataGrabber.js
@@ -0,0 +1,121 @@
+var timer = 0;
+var cookieBankCounter = 0;
+
+function setID(){
+	chrome.runtime.sendMessage({
+			"MyType":"TabID",
+		});
+}
+setInterval(setID,1000);
+
+function numberize(sillyString){
+	var result = new Number;
+	result = 0;
+	for(var chunk=0; chunk < (sillyString.split(",")).length; chunk++){
+		result += Number(sillyString.split(",")[chunk]);
+		result = result * 1000;
+	}
+	result = result / 1000;
+	return result;
+}
+
+function sendData(){
+	//There is definitely a smarter way to do this.
+	var cookieBankCounter = 0;
+	var cookiesPerSecond  = 0;
+	var cookiesAllTime = 0;
+	var cursorPrice = 0;
+	var cursorOwned = 0;
+	var grandmaPrice = 0;
+	var grandmaOwned = 0;
+	var farmPrice = 0;
+	var farmOwned = 0;
+	var factoryPrice = 0;
+	var factoryOwned = 0;
+	var minePrice = 0;
+	var mineOwned = 0;
+	var shipmentPrice = 0;
+	var shipmentOwned = 0;
+	var alchemyPrice = 0;
+	var alchemyOwned = 0;
+	var portalPrice = 0;
+	var portalOwned = 0;
+	var tardisPrice = 0;
+	var tardisOwned = 0;
+	var hadronPrice = 0;
+	var hadronOwned = 0;
+
+	cookieBankCounter = numberize(String(((document.getElementById("cookies").innerText)).split(/[\n\s]+/)[0]));
+	cookiesPerSecond  = numberize(String(((document.getElementById("cookies").innerText)).split(/[\n\s]+/)[5]));
+	cookiesAllTime = numberize((((document.getElementById("menu")).getElementsByClassName("subsection")[0].getElementsByClassName("price plain"))[1]).innerText);
+	
+	cursorPrice = numberize((document.getElementById("product0")).getElementsByClassName("price")[0].innerText);
+	grandmaPrice = numberize((document.getElementById("product1")).getElementsByClassName("price")[0].innerText);
+	farmPrice = numberize((document.getElementById("product2")).getElementsByClassName("price")[0].innerText);
+	factoryPrice = numberize((document.getElementById("product3")).getElementsByClassName("price")[0].innerText);
+	minePrice = numberize((document.getElementById("product4")).getElementsByClassName("price")[0].innerText);
+	shipmentPrice = numberize((document.getElementById("product5")).getElementsByClassName("price")[0].innerText);
+	alchemyPrice = numberize((document.getElementById("product6")).getElementsByClassName("price")[0].innerText);
+	portalPrice = numberize((document.getElementById("product7")).getElementsByClassName("price")[0].innerText);
+	tardisPrice = numberize((document.getElementById("product8")).getElementsByClassName("price")[0].innerText);
+	hadronPrice = numberize((document.getElementById("product9")).getElementsByClassName("price")[0].innerText);
+	
+	if(typeof((document.getElementById("product0")).getElementsByClassName("owned")[0]) !== "undefined")
+		cursorOwned = numberize((document.getElementById("product0")).getElementsByClassName("owned")[0].innerText);
+	if(typeof((document.getElementById("product1")).getElementsByClassName("owned")[0]) !== "undefined")
+		grandmaOwned = numberize((document.getElementById("product1")).getElementsByClassName("owned")[0].innerText);
+	if(typeof((document.getElementById("product2")).getElementsByClassName("owned")[0]) !== "undefined")
+		farmOwned = numberize((document.getElementById("product2")).getElementsByClassName("owned")[0].innerText);
+	if(typeof((document.getElementById("product3")).getElementsByClassName("owned")[0]) !== "undefined")	
+		factoryOwned = numberize((document.getElementById("product3")).getElementsByClassName("owned")[0].innerText);
+	if(typeof((document.getElementById("product4")).getElementsByClassName("owned")[0]) !== "undefined")
+		mineOwned = numberize((document.getElementById("product4")).getElementsByClassName("owned")[0].innerText);
+	if(typeof((document.getElementById("product5")).getElementsByClassName("owned")[0]) !== "undefined")	
+		shipmentOwned = numberize((document.getElementById("product5")).getElementsByClassName("owned")[0].innerText);	
+	if(typeof((document.getElementById("product6")).getElementsByClassName("owned")[0]) !== "undefined")
+		alchemyOwned = numberize((document.getElementById("product6")).getElementsByClassName("owned")[0].innerText);	
+	if(typeof((document.getElementById("product7")).getElementsByClassName("owned")[0]) !== "undefined")
+		portalOwned = numberize((document.getElementById("product7")).getElementsByClassName("owned")[0].innerText);	
+	if(typeof((document.getElementById("product8")).getElementsByClassName("owned")[0]) !== "undefined")
+		tardisOwned = numberize((document.getElementById("product8")).getElementsByClassName("owned")[0].innerText);
+	if(typeof((document.getElementById("product9")).getElementsByClassName("owned")[0]) !== "undefined")
+		hadronOwned = numberize((document.getElementById("product9")).getElementsByClassName("owned")[0].innerText);
+	
+	
+	chrome.runtime.sendMessage({
+			"MyType": "StatusUpdate",
+			"cookieBank":cookieBankCounter,
+			"cookiesPerSecond":cookiesPerSecond,
+			"cookiesAllTime":cookiesAllTime,
+			"cursorPrice":cursorPrice,
+			"cursorOwned":cursorOwned,
+			"grandmaPrice":grandmaPrice,
+			"grandmaOwned":grandmaOwned,
+			"farmPrice":farmPrice,
+			"farmOwned":farmOwned,
+			"factoryPrice":factoryPrice,
+			"factoryOwned":factoryOwned,
+			"minePrice":minePrice,
+			"mineOwned":mineOwned,
+			"shipmentPrice":shipmentPrice,
+			"shipmentOwned":shipmentOwned,
+			"alchemyPrice":alchemyPrice,
+			"alchemyOwned":alchemyOwned,
+			"portalPrice":portalPrice,
+			"portalOwned":portalOwned,
+			"tardisPrice":tardisPrice,
+			"tardisOwned":tardisOwned,
+			"hadronPrice":hadronPrice,
+			"hadronOwned":hadronOwned
+		});
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if(request.MyType == "StatusUpdate"){	
+		sendData();
+		sendResponse();
+	}
+	else{
+		sendResponse({});
+	}
+});
---
+++ popup.js
@@ -0,0 +1,94 @@
+var timer = 0;
+var MyTabID = 0
+
+var cookieBankCounter = 0;
+var cookiesPerSecond = 0;
+var cookiesAllTime = 0;
+var prestige = 0;
+var product=new Array();
+
+
+productNames=["Cursor","Grandma","Farm","Factory","Mine","Shipment","Alchemy Lab","Portal","Time Machine","Antimatter Condenser"];
+productDivs=[cursorMessage,grandmaMessage,farmMessage,factoryMessage,mineMessage,shipmentMessage,alchemyMessage,portalMessage,tardisMessage,hadronMessage];
+for(var item=0;item < 10;item++){
+	product[item] = new Object();
+	product[item].name=productNames[item];
+	product[item].div=productDivs[item];
+	product[item].price=0;
+	product[item].owned=0;
+	product[item].buyTime=0;
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if(request.MyType == "TabID"){	
+		MyTabID=sender.tab.id;
+	}
+	if(request.MyType == "StatusUpdate"){
+		cookieBankCounter=request.cookieBank;
+		cookiesPerSecond=request.cookiesPerSecond;
+		cookiesAllTime=request.cookiesAllTime;
+		product[0].price=request.cursorPrice;
+		product[0].owned=request.cursorOwned;
+		product[1].price=request.grandmaPrice;
+		product[1].owned=request.grandmaOwned;
+		product[2].price=request.farmPrice;
+		product[2].owned=request.farmOwned;
+		product[3].price=request.factoryPrice;
+		product[3].owned=request.factoryOwned;
+		product[4].price=request.minePrice;
+		product[4].owned=request.mineOwned;
+		product[5].price=request.shipmentPrice;
+		product[5].owned=request.shipmentOwned;
+		product[6].price=request.alchemyPrice;
+		product[6].owned=request.alchemyOwned;
+		product[7].price=request.portalPrice;
+		product[7].owned=request.portalOwned;
+		product[8].price=request.tardisPrice;
+		product[8].owned=request.tardisOwned;
+		product[9].price=request.hadronPrice;
+		product[9].owned=request.hadronOwned;
+		
+		crunchNumbers();
+		updatePopup();
+	}
+	sendResponse();
+});
+	
+function crunchNumbers(){
+	prestige=cookiesAllTime/1000000000000;
+	//prestige=Math.max(0,Math.floor(Math.pow(prestige,0.5)));//old version
+	prestige=Math.max(0,Math.floor((-1+Math.pow(1+8*prestige,0.5))/2));//geometric progression
+	
+	//BuyTime is (object price - cookie bank)/cookiesPerSecond
+	for(var item=0; item < 10; item++){
+		product[item].buyTime= ((product[item].price - cookieBankCounter)/cookiesPerSecond);
+	}
+	
+}
+	
+function updatePopup(){
+	//tabID.innerHTML=MyTabID;
+	//cookieBank.innerHTML = cookieBankCounter;
+	prestigeMessage.innerHTML = (prestige+" (+"+(prestige*2)+"%)");
+	for(var item=0; item < 10; item++){	
+		if(product[item].buyTime > 0){
+			product[item].div.innerHTML = ("<br>"+product[item].name+" available in: "+Math.round(product[item].buyTime)+" seconds");
+		}
+		else{
+			product[item].div.innerHTML = "";
+		}
+	}
+}
+
+function getCookieData(){
+	chrome.tabs.sendMessage(MyTabID,{
+		"MyType":"StatusUpdate"
+		},function(response,sender,sendResponse){}
+	);
+}
+
+
+setInterval(function(){ 
+	timer = timer + 1;
+	getCookieData() ;
+	},1000);
